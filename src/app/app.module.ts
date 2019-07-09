f8ddaeac163e66b5bcd1211f53fca925d0202449
workPC <zhzh@tongji.edu.cn>
2019-07-09 08:49:29 +0800

    create first bar chart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from '../routes';
 import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -9,6 +10,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionSalesComponent } from './Sections/section-sales/section-sales.component';
 import { SectionOrdersComponent } from './Sections/section-orders/section-orders.component';
 import { SectionHealthComponent } from './Sections/section-health/section-health.component';
+import { BarChartComponent } from './Charts/bar-chart/bar-chart.component';
+import { LineChartComponent } from './Charts/line-chart/line-chart.component';
+import { PieChartComponent } from './Charts/pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
@@ -17,11 +21,15 @@ import { SectionHealthComponent } from './Sections/section-health/section-health
     SidebarComponent,
     SectionSalesComponent,
     SectionOrdersComponent,
-    SectionHealthComponent
+    SectionHealthComponent,
+    BarChartComponent,
+    LineChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
